a9fe6d9e5bc8d6fde5ad5b43371f41e63c5ab2dd
bh.hwang <bh.hwang@liveklass.com>
2022-08-24 19:57:56 +0900

    V2__add_age_in_member.sql

--- src/main/java/com/example/flyway/entity/Member.java
+++ src/main/java/com/example/flyway/entity/Member.java
@@ -20,4 +20,6 @@ public class Member {
 
     private String name;
 
+    private int age;
+
 }
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,4 +1,4 @@
-create table flyway.MEMBER
+create table MEMBER
 (
     id   bigint auto_increment,
     name varchar(255),
